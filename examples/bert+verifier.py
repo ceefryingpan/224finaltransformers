7bd120cb22d03b4c14fe0f240913f7a22494a494
Charles Pan <cpan22@stanford.edu>
2020-03-13 18:28:40 -0400

    this is going to work

--- examples/bert+verifier.py
+++ examples/bert+verifier.py
@@ -10,7 +10,7 @@ sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 home = os.path.expanduser("~")
 with open(os.path.expanduser('~/224nfinalproject/data/dev-v2.0.json')) as reader:
     data = json.load(reader)["data"]
-with open(os.path.expanduser('~/224finaltransformers/examples/outputs/albertforqa/nbest_predictions_.json')) as verifier:
+with open(os.path.expanduser('~/224finaltransformers/examples/output/albertforqa/nbest_predictions_.json')) as verifier:
     qamodel = json.load(verifier)
 verifiertokenizer = AlbertTokenizer.from_pretrained('~/224finaltransformers/examples/outputs/albertforsqc')
 verifiermodel = AlbertForSequenceClassification.from_pretrained('~/224finaltransformers/examples/outputs/albertforsqc')
